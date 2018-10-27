83b8e6d0b1b5e4d0c0c23e0397ebfe36ef3e05cf
李杰 <lijie4@tuandai.com>
2018-10-27 22:13:11 +0800

    Modify String+Localized

--- SwiftToolBox/Extensions/String/String+Localized.swift
+++ SwiftToolBox/Extensions/String/String+Localized.swift
@@ -6,12 +6,14 @@
 //  Copyright © 2018年 李杰. All rights reserved.
 //
 
+import Foundation
+
 extension String {
     /// According to the key to get localized string
     ///
     /// - Parameter key: key
     /// - Returns: String
-    class func localized(_ key: String) -> String {
+    static func localized(_ key: String) -> String {
         return NSLocalizedString(key, comment: "")
     }
 }

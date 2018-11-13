f90633921558b237c78a8c842f6e4235b7cfa23c
李杰 <lijie4@tuandai.com>
2018-11-13 22:06:06 +0800

    Add UITapGestureRecognizer

---
+++ SwiftToolBox/Extensions/UITapGestureRecognizer/UITapGestureRecognizer+Extension.swift
@@ -0,0 +1,44 @@
+//
+//  UITapGestureRecognizer+Extension.swift
+//  SwiftToolBox
+//
+//  Created by 李杰 on 2018/11/13.
+//  Copyright © 2018年 李杰. All rights reserved.
+//
+
+import UIKit
+
+extension UITapGestureRecognizer {
+    /// Tap AttributedText in label --- UILabel 里点击带属性的字符串
+    ///
+    /// - Parameters:
+    ///   - label: UILabel
+    ///   - targetRange: NSRange
+    /// - Returns: Bool
+    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
+        // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: CGSize.zero)
+        let textStorage = NSTextStorage(attributedString: label.attributedText!)
+        
+        // Configure layoutManager and textStorage
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+        
+        // Configure textContainer
+        textContainer.lineFragmentPadding = 0.0
+        textContainer.lineBreakMode = label.lineBreakMode
+        textContainer.maximumNumberOfLines = label.numberOfLines
+        let labelSize = label.bounds.size
+        textContainer.size = labelSize
+        
+        // Find the tapped character location and compare it to the specified range
+        let locationOfTouchInLabel = self.location(in: label)
+        let textBoundingBox = layoutManager.usedRect(for: textContainer)
+        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x, y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y)
+        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y: locationOfTouchInLabel.y - textContainerOffset.y)
+        let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        
+        return NSLocationInRange(indexOfCharacter, targetRange)
+    }
+}
--- SwiftToolBox/Swizzle/TestSwizzleViewController.swift
+++ SwiftToolBox/Swizzle/TestSwizzleViewController.swift
@@ -25,39 +25,3 @@ class TestSwizzleViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 }
-
-extension UIViewController: SelfAwake {
-    static func awake() {
-//        print(self.description())
-        UIViewController.classInit()
-    }
-    
-    static func classInit() {
-        swizzleMethod
-    }
-    
-    private static let swizzleMethod: Void = {
-        let originalSelector = #selector(viewWillAppear(_:))
-        let swizzledSelector = #selector(swizzled_viewWillAppear(_:))
-        swizzlingForClass(UIViewController.self, originalSelector: originalSelector, swizzledSelector: swizzledSelector)
-    }()
-    
-    private static func swizzlingForClass(_ forClass: AnyClass, originalSelector: Selector, swizzledSelector: Selector) {
-        let originalMethod = class_getInstanceMethod(forClass, originalSelector)
-        let swizzledMethod = class_getInstanceMethod(forClass, swizzledSelector)
-        guard (originalMethod != nil && swizzledMethod != nil) else {
-            return
-        }
-        if class_addMethod(forClass, originalSelector, method_getImplementation(swizzledMethod!), method_getTypeEncoding(swizzledMethod!)) {
-            class_replaceMethod(forClass, swizzledSelector, method_getImplementation(originalMethod!), method_getTypeEncoding(originalMethod!))
-        } else {
-            method_exchangeImplementations(originalMethod!, swizzledMethod!)
-        }
-    }
-    
-    /// swizzle
-    @objc func swizzled_viewWillAppear(_ animated: Bool) {
-        swizzled_viewWillAppear(animated)
-        print("swizzled_viewWillAppear")
-    }
-}
